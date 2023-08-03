2567c9a9c618a88ef0a8611a4fc89a5c74b5ce36
julian armellla <juena@julian77.julian77>
2023-08-02 21:21:45 -0300

    agregue el proyecto de mercado liebre

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+app.use(express.static("public"))
+
+app.listen(3000, () => {console.log('todo bien')
+})
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'))
+})
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/login.html"))
+})
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/register.html"))
+})
